997abffceafa7257ccbb892111424b8514784341
xprograms24 <cexcel58@gmail.com>
2025-09-02 00:06:52 +0100

    adding new menu

--- Electricitybillingsystem/testfolder/main.py
+++ Electricitybillingsystem/testfolder/main.py
@@ -14,12 +14,33 @@ mydb = pymysql.connect(
 mycursor = mydb.cursor()
 print("db connected to successfully")
 
+def customer_menu():
+    print("\n======customer management======")
+    print("1.Add new customers")
+    print("2.update a customers information")
+    print("3.view all customers")
+    print("4 exit customer managment")
+
+    while True:
+        option = input("\nselect an option: ")
+        if option == "1":
+            print("add customer")
+        elif option == "2":
+            print("update customer")
+        elif option == "3":
+            print("view all customers")
+        elif option == '4':
+            print("thank you for using customer managment")
+            break
+        else:
+            print("invalid")
+
 #create menu option
 
 while True:
     print("=====================welcome to E-electricity Admin option===========================")
-    print("\n please select an option")
-    print("1.Customer managment")
+    print("please select an option")
+    print("\n1.Customer managment")
     print("2.Billing managment")
     print("3.payment Managment")
     print("4.report analysis")
@@ -28,7 +49,7 @@ while True:
     choice = input("Enter your choice: ")
 
     if choice == '1':
-        pass
+            customer_menu()
     elif choice == '2':
         pass
     elif choice == '3':
