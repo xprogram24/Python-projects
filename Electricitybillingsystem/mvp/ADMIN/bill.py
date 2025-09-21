13320ada392126245aacb345c698f6b432b1ad24
xprogram24 <cexcel58@gmail.com>
2025-09-21 13:19:19 +0100

    ..

--- Electricitybillingsystem/mvp/ADMIN/bill.py
+++ Electricitybillingsystem/mvp/ADMIN/bill.py
@@ -2,7 +2,8 @@
 import pymysql
 import time
 from tabulate import tabulate
-mydb = pymysql.connect(
+try :
+    mydb = pymysql.connect(
     host="localhost",
     port=3306, 
     user="admin",
@@ -11,115 +12,116 @@ mydb = pymysql.connect(
 )
 
 #create cursor
-mycursor = mydb.cursor()
-print("db connected to successfully")
+    mycursor = mydb.cursor()
+    print("db connected to successfully")
 
-def generate_bill():
-    print("welcome to bill generating")
-    meter_number = input("please input meter number :").strip()
+    def generate_bill():
+        print("welcome to bill generating")
+        meter_number = input("please input meter number :").strip()
+        
+        mycursor.execute("SELECT * FROM Customer WHERE meter_number = %s", (meter_number))
+        result = mycursor.fetchall()
+
+        if result:
+            query = "SELECT customer_id FROM Customer WHERE meter_number = %s"
+            mycursor.execute(query,meter_number)
+
+            #ADD an if statement to check if meter number exists
+            customers = mycursor.fetchone()
+        
+            customer_id = customers[0]
+
+            billing_month = input("input billing month: ").strip()
+            unit = int(input("input unit consumed: "))
+            rate = 66.00
+        
+            bill = unit * rate 
+            VAT = 0.075 * bill
+            total_amount = bill + VAT
+            print(f"bill is {bill}")
+            print(f"total bill + vat is {total_amount}")
+            bill_query = "INSERT INTO Bills (customer_id, units_used, rate_per_unit, total_amount, billing_month,Payment_STATUS) VALUES (%s,%s,%s,%s,%s,'unpaid')"
+            mycursor.execute(bill_query,(customer_id,unit,rate,total_amount,billing_month))
+            mydb.commit()
+            time.sleep(2)
+            print(f"✅ Bill generated for {meter_number} | Amount: ₦{total_amount} ")
+        else:
+            print("meter number does not exist")
+
+    def update_bill():
+        meter_number = input("enter a meter number: ").strip()
     
-    mycursor.execute("SELECT * FROM Customer WHERE meter_number = %s", (meter_number))
-    result = mycursor.fetchall()
 
-    if result:
-        query = "SELECT customer_id FROM Customer WHERE meter_number = %s"
-        mycursor.execute(query,meter_number)
-
-        #ADD an if statement to check if meter number exists
-        customers = mycursor.fetchone()
-    
-        customer_id = customers[0]
-
-        billing_month = input("input billing month: ").strip()
-        unit = int(input("input unit consumed: "))
-        rate = 66.00
-    
-        bill = unit * rate 
-        VAT = 0.075 * bill
-        total_amount = bill + VAT
-        print(f"bill is {bill}")
-        print(f"total bill + vat is {total_amount}")
-        bill_query = "INSERT INTO Bills (customer_id, units_used, rate_per_unit, total_amount, billing_month,Payment_STATUS) VALUES (%s,%s,%s,%s,%s,'unpaid')"
-        mycursor.execute(bill_query,(customer_id,unit,rate,total_amount,billing_month))
-        mydb.commit()
-        time.sleep(2)
-        print(f"✅ Bill generated for {meter_number} | Amount: ₦{total_amount} ")
-    else:
-        print("meter number does not exist")
-
-def update_bill():
-    meter_number = input("enter a meter number: ").strip()
-  
-
-    searchQuery = "SELECT Customer.customer_id, Customer.fullName, Customer.meter_number, Bills.bill_id, Bills.billing_month, Bills.units_used, Bills.rate_per_unit, Bills.total_amount,Bills.Payment_STATUS FROM Bills INNER JOIN  Customer ON Bills.customer_id = Customer.customer_id WHERE Customer.meter_number = %s AND Bills.Payment_STATUS = %s"
-    mycursor.execute(searchQuery,(meter_number,"unpaid"))
-    customers = mycursor.fetchall()
-    header = ["Customer_id","Full Name","Meter number", "Bill ID","Billing Month","units_used","Rate","Total Amount (#)","Payment_STATUS"]
-    print(tabulate(customers,headers=header, tablefmt="fancy_grid"))
-    
-    if not customers:
-        print(" no unpaid bills found")
-    else:
-        print("update unit used and billing month")
-        newUnit = float(input("input unit used: "))
-        newMonth = input("type  month : ")
-        rate = 66.00
-    
-        bill = newUnit * rate 
-        VAT = 0.075 * bill
-        total_amount = bill + VAT
-        #get billl id
-        MYbill_id = customers[0][3]
-        update = "UPDATE  Bills SET Billing_month = %s , units_used = %s,total_amount = %s WHERE bill_id = %s"
-        mycursor.execute(update,(newMonth,newUnit,total_amount, MYbill_id,))
-        mydb.commit()
-
-        print("change sucessful")
         searchQuery = "SELECT Customer.customer_id, Customer.fullName, Customer.meter_number, Bills.bill_id, Bills.billing_month, Bills.units_used, Bills.rate_per_unit, Bills.total_amount,Bills.Payment_STATUS FROM Bills INNER JOIN  Customer ON Bills.customer_id = Customer.customer_id WHERE Customer.meter_number = %s AND Bills.Payment_STATUS = %s"
         mycursor.execute(searchQuery,(meter_number,"unpaid"))
         customers = mycursor.fetchall()
-        header = ["Customer_id","Full Name","Meter number", "Bill ID","Billing Month","units_used","Rate (#)","Total Amount (#)","Payment_STATUS"]
+        header = ["Customer_id","Full Name","Meter number", "Bill ID","Billing Month","units_used","Rate","Total Amount (#)","Payment_STATUS"]
         print(tabulate(customers,headers=header, tablefmt="fancy_grid"))
+        
+        if not customers:
+            print(" no unpaid bills found")
+        else:
+            print("update unit used and billing month")
+            newUnit = float(input("input unit used: "))
+            newMonth = input("type  month : ")
+            rate = 66.00
+        
+            bill = newUnit * rate 
+            VAT = 0.075 * bill
+            total_amount = bill + VAT
+            #get billl id
+            MYbill_id = customers[0][3]
+            update = "UPDATE  Bills SET Billing_month = %s , units_used = %s,total_amount = %s WHERE bill_id = %s"
+            mycursor.execute(update,(newMonth,newUnit,total_amount, MYbill_id,))
+            mydb.commit()
 
-def view_bills():
-    #add an if to check if meter number exist
-    while True:
-        print("\n1.view all bills")
-        print("2.View a specific bill")
-        print("3.EXIT")
-
-        option = input("select an option: ").strip()
-        if option == "1":
-            searchQuery = "SELECT Customer.customer_id, Customer.fullName, Customer.meter_number, Bills.bill_id, Bills.billing_month, Bills.units_used, Bills.rate_per_unit, Bills.total_amount,Bills.Payment_STATUS FROM Bills INNER JOIN  Customer ON Bills.customer_id = Customer.customer_id "
-            mycursor.execute(searchQuery)
+            print("change sucessful")
+            searchQuery = "SELECT Customer.customer_id, Customer.fullName, Customer.meter_number, Bills.bill_id, Bills.billing_month, Bills.units_used, Bills.rate_per_unit, Bills.total_amount,Bills.Payment_STATUS FROM Bills INNER JOIN  Customer ON Bills.customer_id = Customer.customer_id WHERE Customer.meter_number = %s AND Bills.Payment_STATUS = %s"
+            mycursor.execute(searchQuery,(meter_number,"unpaid"))
             customers = mycursor.fetchall()
-            time.sleep(1)
-            header = ["Customer_id","Full Name","Meter number", "Bill ID","Billing Month","units_used","Rate","Total Amount (#)","Payment_STATUS"]
+            header = ["Customer_id","Full Name","Meter number", "Bill ID","Billing Month","units_used","Rate (#)","Total Amount (#)","Payment_STATUS"]
             print(tabulate(customers,headers=header, tablefmt="fancy_grid"))
-    
-        elif option == "2": 
-            Meter_Nnumber = input("input meter Number: ").strip()
-            mycursor.execute("SELECT * FROM Customer WHERE meter_number = %s", (Meter_Nnumber))
-            result = mycursor.fetchall()
 
-            if result:
-                searchQuery = "SELECT Customer.customer_id, Customer.fullName, Customer.meter_number, Bills.bill_id, Bills.billing_month, Bills.units_used, Bills.rate_per_unit, Bills.total_amount,Bills.Payment_STATUS FROM Bills INNER JOIN  Customer ON Bills.customer_id = Customer.customer_id WHERE Customer.meter_number LIKE  %s"
-                mycursor.execute(searchQuery,("%" + Meter_Nnumber + "%"))
+    def view_bills():
+        #add an if to check if meter number exist
+        while True:
+            print("\n1.view all bills")
+            print("2.View a specific bill")
+            print("3.EXIT")
+
+            option = input("select an option: ").strip()
+            if option == "1":
+                searchQuery = "SELECT Customer.customer_id, Customer.fullName, Customer.meter_number, Bills.bill_id, Bills.billing_month, Bills.units_used, Bills.rate_per_unit, Bills.total_amount,Bills.Payment_STATUS FROM Bills INNER JOIN  Customer ON Bills.customer_id = Customer.customer_id "
+                mycursor.execute(searchQuery)
                 customers = mycursor.fetchall()
                 time.sleep(1)
-                header = ["Customer_id","Full Name","Meter number", "Bill ID","Billing Month","units_used","Rate","Total Amount (#)","payment Status"]
+                header = ["Customer_id","Full Name","Meter number", "Bill ID","Billing Month","units_used","Rate","Total Amount (#)","Payment_STATUS"]
                 print(tabulate(customers,headers=header, tablefmt="fancy_grid"))
+        
+            elif option == "2": 
+                Meter_Nnumber = input("input meter Number: ").strip()
+                mycursor.execute("SELECT * FROM Customer WHERE meter_number = %s", (Meter_Nnumber))
+                result = mycursor.fetchall()
+
+                if result:
+                    searchQuery = "SELECT Customer.customer_id, Customer.fullName, Customer.meter_number, Bills.bill_id, Bills.billing_month, Bills.units_used, Bills.rate_per_unit, Bills.total_amount,Bills.Payment_STATUS FROM Bills INNER JOIN  Customer ON Bills.customer_id = Customer.customer_id WHERE Customer.meter_number LIKE  %s"
+                    mycursor.execute(searchQuery,("%" + Meter_Nnumber + "%"))
+                    customers = mycursor.fetchall()
+                    time.sleep(1)
+                    header = ["Customer_id","Full Name","Meter number", "Bill ID","Billing Month","units_used","Rate","Total Amount (#)","payment Status"]
+                    print(tabulate(customers,headers=header, tablefmt="fancy_grid"))
+                else:
+                    print("meter number not found")
+
+            elif option == "3":
+                print("thank you for using bill managment")
+                break
             else:
-                print("meter number not found")
+                print("invalid")
 
-        elif option == "3":
-            print("thank you for using bill managment")
-            break
-        else:
-            print("invalid")
+except Exception as e:
+    print("Database connection failed",e)
 
 
 
-
-
-   
+    
--- Electricitybillingsystem/mvp/ADMIN/main.py
+++ Electricitybillingsystem/mvp/ADMIN/main.py
@@ -6,144 +6,147 @@ from bill import generate_bill,view_bills,update_bill
 from payment import view_payment,generatepdf,tracking
 from reportAnalysis import Totalrevenue,outstanding_balance,monthly_usage
 import time
-mydb = pymysql.connect(
-    host="localhost",
-    port=3306,
-    user="admin",
-    password = "myadmin",
-    database="electricBilling_DB"
-)
+try:
+    mydb = pymysql.connect(
+        host="localhost",
+        port=3306,
+        user="admin",
+        password = "myadmin",
+        database="electricBilling_DB"
+    )
 
-#create cursor
-mycursor = mydb.cursor()
-print("db connected to successfully")
-
-
-#function to display customer menu
-def customer_menu():
-
-    while True:
-        print("\n======customer management======")
-        print("1.Add new customers")
-        print("2.update a customers information")
-        print("3.view all customers")
-        print("4 exit customer managment")
-
-        option = input("\nselect an option: ")
-        if option == "1":
-            addCustomer()
-        elif option == "2":
-            updateCustomer()
-        elif option == "3":
-            viewCustomer()
-        elif option == '4':
-            print("thank you for using customer managment")
-            break
-        else:
-            print("invalid")
-
-#function to display bill menu
-def bill_menu():
-    
-
-    while True:
-
-        print("\n======Billing management======")
-        print("1.Generate bills for each customer")
-        print("2.update bills if mistakes happen")
-        print("3.view all bills")
-        print("4 exit billing managment")
-
-        option = input("\nselect an option: ")
-        if option == "1":
-            print("generate bills ")
-            generate_bill()
-        elif option == "2":
-           update_bill()
-        elif option == "3":
-            
-            view_bills()
-        elif option == '4':
-            print("thank you for using bill managment")
-            break
-        else:
-            print("invalid")
+    #create cursor
+    mycursor = mydb.cursor()
+    print("db connected to successfully")
 
 
+    #function to display customer menu
+    def customer_menu():
 
-#function to display payment menu
-def payment_managment():
-    
+        while True:
+            print("\n======customer management======")
+            print("1.Add new customers")
+            print("2.update a customers information")
+            print("3.view all customers")
+            print("4 exit customer managment")
 
-    while True:
+            option = input("\nselect an option: ")
+            if option == "1":
+                addCustomer()
+            elif option == "2":
+                updateCustomer()
+            elif option == "3":
+                viewCustomer()
+            elif option == '4':
+                print("thank you for using customer managment")
+                break
+            else:
+                print("invalid")
 
-        print("\n======payment management======")
-        print("1.view payment ")
-        print("2.Tracking pending/unpaid bills")#optional
-        print("3.Generate reciepts(PDF)")
-        print("4 exit payment managment")
+    #function to display bill menu
+    def bill_menu():
+        
 
-        option = input("\nselect an option: ")
-        if option == "1":
-            print("payment record")
-            view_payment()
-        elif option == "2":
-            tracking()
-        elif option == "3":
-            generatepdf()
-        elif option == '4':
-            print("thank you for using payment managment")
-            break
-        else:
-            print("invalid")
+        while True:
 
-#function to display report analysis menu
-def analysis():
+            print("\n======Billing management======")
+            print("1.Generate bills for each customer")
+            print("2.update bills if mistakes happen")
+            print("3.view all bills")
+            print("4 exit billing managment")
 
-    while True:
-        time.sleep(3)
-        print("\n======Report & Analytics======")
-        print("1.Total revenue collected")
-        print("2.outstanding balance")
-        print("3.monthly usage trend")
-        print("4 exit Report & analytics")
+            option = input("\nselect an option: ")
+            if option == "1":
+                print("generate bills ")
+                generate_bill()
+            elif option == "2":
+                update_bill()
+            elif option == "3":
+                
+                view_bills()
+            elif option == '4':
+                print("thank you for using bill managment")
+                break
+            else:
+                print("invalid")
 
-        option = input("\nselect an option: ")
-        if option == "1":
-            Totalrevenue()
-        elif option == "2":
-            outstanding_balance()
-        elif option == "3":
-            monthly_usage()
-        elif option == '4':
-            print("thank you for report & analytics")
-            break
-        else:
-            print("invalid")
 
-# main menu option
-def menu():
-    while True:
-        print("=====================welcome to E-electricity Admin option===========================")
-        print("please select an option")
-        print("\n1.Customer managment")
-        print("2.Billing managment")
-        print("3.payment Managment")
-        print("4.report analysis")
-        print("5.Exit")
 
-        choice = input("Enter your choice: ")
+    #function to display payment menu
+    def payment_managment():
+        
 
-        if choice == '1':
-            customer_menu()
-        elif choice == '2':
-            bill_menu()
-        elif choice == '3':
-            payment_managment()
-        elif choice == '4':
-            analysis()
-        elif choice == '5':
-            print("goodbye")
-            break
-        else :
-            print("invalid option")
+        while True:
+
+            print("\n======payment management======")
+            print("1.view payment ")
+            print("2.Tracking pending/unpaid bills")#optional
+            print("3.Generate reciepts(PDF)")
+            print("4 exit payment managment")
+
+            option = input("\nselect an option: ")
+            if option == "1":
+                print("payment record")
+                view_payment()
+            elif option == "2":
+                tracking()
+            elif option == "3":
+                generatepdf()
+            elif option == '4':
+                print("thank you for using payment managment")
+                break
+            else:
+                print("invalid")
+
+    #function to display report analysis menu
+    def analysis():
+
+        while True:
+            time.sleep(2)
+            print("\n======Report & Analytics======")
+            print("1.Total revenue collected")
+            print("2.outstanding balance")
+            print("3.monthly usage trend")
+            print("4 exit Report & analytics")
+
+            option = input("\nselect an option: ")
+            if option == "1":
+                Totalrevenue()
+            elif option == "2":
+                outstanding_balance()
+            elif option == "3":
+                monthly_usage()
+            elif option == '4':
+                print("thank you for report & analytics")
+                break
+            else:
+                print("invalid")
+
+    # main menu option
+    def menu():
+        while True:
+            print("=====================welcome to E-electricity Admin option===========================")
+            print("please select an option")
+            print("\n1.Customer managment")
+            print("2.Billing managment")
+            print("3.payment Managment")
+            print("4.report analysis")
+            print("5.Exit")
+
+            choice = input("Enter your choice: ")
+
+            if choice == '1':
+                customer_menu()
+            elif choice == '2':
+                bill_menu()
+            elif choice == '3':
+                payment_managment()
+            elif choice == '4':
+                analysis()
+            elif choice == '5':
+                print("goodbye")
+                break
+            else :
+                print("invalid option")
+except Exception as e:
+    print("database connection failed",e)
